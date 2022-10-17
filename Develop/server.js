e717b2ecd41b41bb4a6f95579cbdc41f6583a8bc
Strongie <nickstrong665@gmail.com>
2022-10-17 11:34:14 +1100

    setup the standard apps and consts

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
