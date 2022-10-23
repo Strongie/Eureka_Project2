d4b864d6dd8daac69df40186f43e5e938ed1aabd
Strongie <nickstrong665@gmail.com>
2022-10-23 11:28:09 +1100

    require db.json to server.js

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -3,5 +3,10 @@ var app = modules.exports = express();
 const path = require('path');
 const fs = require('fs');
 
+app.get('./routes/apiroutes.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+}
 
-
+app.post('./routes/apiroutes.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+} 
