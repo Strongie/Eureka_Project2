58900f8f13ffac722b140d39bd2945df424d0618
Strongie <nickstrong665@gmail.com>
2022-10-23 21:43:34 +1100

    added require for htmlroutes

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,8 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 const db = require('./db/db.json');
-
+const apiroutes = require('./routes/apiroutes.js');
+const htmlroutes = require('./routes/htmlroutes.js');
 
 const PORT = process.env.port || 3001;
 
